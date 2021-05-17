ef76bd32a50427f9e81c2800522e27f53ab38333
themuplugs <shagor@themeplugs.com>
2021-05-17 22:36:30 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
           },
         },
         // url: process.env.WPGRAPHQL_URL,
-        url: `https://cuteanimalplanet.com/graphql`,
+        url: `https://www.cuteanimalplanet.com/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: false,
