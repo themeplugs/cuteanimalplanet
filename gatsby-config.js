89a5f5071d64971c77995e0fe5713573d2849356
developershagor <shagor@themeplugs.com>
2021-05-21 02:57:56 +0600

    sdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
           },
         },
         // url: process.env.WPGRAPHQL_URL,
-        url: `https://www.cuteanimalplanet.com/graphql`,
+        url: `http://themeplugs.com/plugin/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: false,
